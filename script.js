d1a17dc9fccd1e955c8fc462e876b2f75e558107
Tanha Islam Sarani <tislam5667@gmail.com>
2025-05-13 21:04:47 +0100

    Made small changes

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  // Open the project panel
+function toggleProjects() {
+  document.getElementById('projectPanel').classList.add('show');
+}
+
+// Close the project panel (when the "×" is clicked)
+function closeProjects() {
+  document.getElementById('projectPanel').classList.remove('show');
+}
